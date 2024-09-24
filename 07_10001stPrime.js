b05869e68d1a77b9a77261b18e75bbdb7e203b23
Ignas <ignui.c@gmail.com>
2024-09-24 09:59:49 +0100

    07_10001stPrime.js code clean up
    Just some minor code clean up for better visual

--- 07_10001stPrime.js
+++ 07_10001stPrime.js
@@ -3,72 +3,57 @@
 nthNumber = 10001
 
 function isPrimeNumber(number){
-/*
-check if the number is prime
-*/
-
+    /*
+    check if the number is prime
+    */
     for(i = 2; i < number; i++){
-
         if(number % i === 0){
             return false
         }
     }
 
     return true
-
 }
 
 function findNextPrime(number){
-/*
-find the next prime number that is higher than supplied parameter
-*/
-
-let nextNumber = number + 1
-
-let loopBreaker = 20000 // I hate infinite loops, so just in case...
+    /*
+    find the next prime number that is higher than supplied parameter
+    */
+    let nextNumber = number + 1
+    let loopBreaker = 20000 // I hate infinite loops, so just in case...
 
     while(true){
+        
         if(isPrimeNumber(nextNumber)){
             return nextNumber
         } else {
-
             nextNumber += 1
-
             loopBreaker -= 1
 
             // safety condition to prevent being stuck in infinite loop
             if(loopBreaker == 0){
-
                 console.log("Infinite loop broken at number " + nextNumber)
                 return nextNumber
-
             }
         }
     }
 }
 
 function findnthPrime(number){
-/*
-find the n'th prime number, where n is the supplied parameter
-*/
-
-let nthPrime = 0
-
-let counter = 0
+    /*
+    find the n'th prime number, where n is the supplied parameter "number"
+    */
+    let nthPrime = 0
+    let counter = 0
 
     for(n = 0; n <= number; n++){
-
         nthPrime = findNextPrime(nthPrime)
         counter = n
-
     }
 
     console.log("------------------------------------")
     console.log("Prime number " + counter + " found: " + nthPrime)
 }
 
- 
-
 // run solution
-
 findnthPrime(nthNumber)
