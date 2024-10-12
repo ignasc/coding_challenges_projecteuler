c91476ac0d519be2f031664dc4b447990301f140
ignasc <ignui.c@gmail.com>
2024-10-12 11:42:08 +0100

    Euler problem #10 solved. Two methods.

---
+++ 10_summationOfPrimes_FAST.js
@@ -0,0 +1,76 @@
+//NOTE: fast method. Took about 0.1 second on my machine to complete.
+
+let numberOfDataPoints = 2000000
+let resultSum = 0
+let dataArray
+let dataIndexArray
+
+// function to build an array of all the numbers between 2 and two million.
+function buildArray(limit, startingNumber = 2){
+    let generatedArray = []
+
+    for(let i = startingNumber; i <= limit; i++){
+        generatedArray.push(i)
+    }
+    return generatedArray
+
+}
+
+function isPrimeNumber(number){
+    /*
+    function to check if the number is prime
+    */
+    for(i = 2; i < number; i++){
+        if(number % i === 0){
+            return false
+        }
+    }
+    
+    return true
+
+}
+
+function main(){
+    /*
+    Function that checks for prime number in data array. Uses https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes method to flag numbers that are not prime. To make it work with single array, each element, that is NOT a prime, will be replaced with -1. Then we simply add up all numbers that are positive, since those are left as prime numbers.
+    */
+    let startTime = null
+    let endTime = null
+    startTime = Date.now()
+    dataArray = buildArray(numberOfDataPoints) // generate data array
+
+    // since every prime multiple is NOT a prime, we only check numbers up to square root of two million.
+    let iterationLimit = Math.sqrt(dataArray.length)
+
+    // iterate through every number that is not flagged as negative (not a prime)
+    for(let numberIndex = 0; numberIndex <= iterationLimit; numberIndex++){
+        if(dataArray[numberIndex] < 0){continue}// if it is flagged as NOT prime, skip it
+            let numberIsPrime = isPrimeNumber(dataArray[numberIndex])
+            // check if number is prime
+            if(numberIsPrime){
+                let primeNumber = dataArray[numberIndex]
+                let primeIndex = numberIndex
+                
+                // every multiple of prime number will NOT be a prime number, so we flag those to avoid checking them again later
+                for(let j = primeNumber + primeIndex; j < dataArray.length; j = j + primeNumber){
+                    dataArray[j] = -1
+                }
+            }
+        }
+        
+        // add up all prime numbers (the ones that are not flagged as -1)
+        for(let i = 0; i < dataArray.length; i++){
+            if(dataArray[i] > 0){
+                resultSum += dataArray[i]
+            }
+        }
+        
+        console.log("The sum of all prime numbers up to " + numberOfDataPoints + " is: " + resultSum)
+        endTime = Date.now()
+        console.log("Operation completed in " + (endTime - startTime)/1000 + " seconds")
+        resultSum = 0 // reset the sum (delete once both calculation methods are separated
+
+}
+
+// run the function with problem solution
+main()
---
+++ 10_summationOfPrimes_SLOW.js
@@ -0,0 +1,59 @@
+//NOTE: slow method. Took 476 seconds on my machine to complete.
+
+let numberOfDataPoints = 2000000
+let resultSum = 0
+let dataArray
+let dataIndexArray
+
+// function to build an array of all the numbers between 2 and two million.
+function buildArray(limit, startingNumber = 2){
+    let generatedArray = []
+
+    for(let i = startingNumber; i <= limit; i++){
+        generatedArray.push(i)
+    }
+    return generatedArray
+
+}
+
+function isPrimeNumber(number){
+    /*
+    function to check if the number is prime
+    */
+    for(i = 2; i < number; i++){
+        if(number % i === 0){
+            return false
+        }
+    }
+    
+    return true
+
+}
+
+function main(){
+    /*
+    Function that iterates through every number in the data array and checks if it is a prime number and adds it to the total sum.
+    */
+   let startTime = null // to store time before starting operation
+   let endTime = null // to store time after finishing operation
+   dataArray = buildArray(numberOfDataPoints) // generate data array
+   startTime = Date.now()
+
+   // check every number in data array if it is prime
+   for(let i = 0; i < dataArray.length; i++){
+        let isPrime = isPrimeNumber(dataArray[i])
+        // if prime, then add it to total sum
+        if(isPrime){
+            resultSum += dataArray[i]
+        }
+    }
+
+    console.log("The sum of all prime numbers up to " + numberOfDataPoints + " is: " + resultSum)
+    endTime = Date.now()
+    console.log("Operation completed in " + (endTime - startTime)/1000 + " seconds")
+    resultSum = 0 // reset the sum (delete once both calculation methods are separated
+
+}
+
+// run the function with problem solution
+main()
